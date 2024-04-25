73ecc0d009e612d2a2fba0cf2b3b57c1a9aef6a3
carlos <carlosbonfim722@gmail.com>
2024-04-25 19:12:03 -0300

    version-final

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var swiper = new Swiper(".curso-slide", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+       
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+  var swiper = new Swiper(".prof-slides", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+       
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+  var swiper = new Swiper(".review-slides", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+       
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
